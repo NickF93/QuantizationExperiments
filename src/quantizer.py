6b2a270b1ed585ad6bd9ef94d25b050e8b6494a1
Niccolò Ferrari <niccolo.ferrari@unife.it>
2024-08-31 11:25:40 +0200

    quantization experiments

--- src/quantizer.py
+++ src/quantizer.py
@@ -16,3 +16,37 @@ def linear_q_with_scale_and_zero_point(tensor, scale, zero_point, dtype = torch.
 def linear_dequantizer(quantized_tensor, scale, zero_point):
     dequantized_tensor = scale * (quantized_tensor.float() - zero_point)
     return dequantized_tensor
+
+def q_min_max(dtype=torch.int8):
+    return torch.iinfo(dtype).min, torch.iinfo(dtype).max
+
+def get_q_scale_and_zero_point(tensor, dtype=torch.int8):
+    
+    q_min, q_max = q_min_max(dtype)
+    r_min, r_max = tensor.min().item(), tensor.max().item()
+
+    scale = (r_max - r_min) / (q_max - q_min)
+
+    zero_point = q_min - (r_min / scale)
+
+    # clip the zero_point to fall in [quantized_min, quantized_max]
+    if zero_point < q_min:
+        zero_point = q_min
+    elif zero_point > q_max:
+        zero_point = q_max
+    else:
+        # round and cast to int
+        zero_point = int(round(zero_point))
+    
+    return scale, zero_point
+
+def linear_quantizer(tensor, dtype=torch.int8):
+    scale, zero_point = get_q_scale_and_zero_point(tensor, 
+                                                   dtype=dtype)
+    
+    quantized_tensor = linear_q_with_scale_and_zero_point(tensor,
+                                                          scale, 
+                                                          zero_point, 
+                                                          dtype=dtype)
+    
+    return quantized_tensor, scale , zero_point
--- tests/quantizer_tests.py
+++ tests/quantizer_tests.py
@@ -1,7 +1,8 @@
 import unittest
 import torch
+import numpy as np
 import logging
-from src.quantizer import linear_q_with_scale_and_zero_point, linear_dequantizer
+from src.quantizer import linear_q_with_scale_and_zero_point, linear_dequantizer, get_q_scale_and_zero_point, linear_quantizer
 from src.helper import plot_quantization_errors
 
 # Configure logging
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 class TestQuantizer(unittest.TestCase):
 
     def setUp(self):
-        torch.seed(123)
+        torch.manual_seed(123)
 
         ### a dummy tensor to test the implementation
         self.test_tensor = torch.tensor(
@@ -28,6 +29,7 @@ class TestQuantizer(unittest.TestCase):
              dtype=torch.int8)
 
     def test_quantizer(self):
+        logger.warning('test_quantizer')
 
         ### these are random values for "scale" and "zero_point"
         ### to test the implementation
@@ -45,6 +47,7 @@ class TestQuantizer(unittest.TestCase):
         self.assertTrue(torch.equal(self.quantized_tensor, quantized_tensor))
 
     def test_dequantizer(self):
+        logger.warning('test_dequantizer')
 
         ### these are random values for "scale" and "zero_point"
         ### to test the implementation
@@ -66,6 +69,43 @@ class TestQuantizer(unittest.TestCase):
 
         # Use torch.allclose for floating-point comparisons
         self.assertTrue(torch.allclose(tensor, dequantized_tensor, atol=1e-9))
+    
+    def test_newscale(self):
+        logger.warning('test_newscale')
+
+        new_scale, new_zero_point = get_q_scale_and_zero_point(self.test_tensor)
+
+        scale = 3.58
+        zero_point = -77
+
+        logger.info('new_scale: %s', new_scale)
+        logger.info('new_zero_point: %s', new_zero_point)
+
+        logger.info('SCALE ERR: %s', np.abs(scale - new_scale))
+        logger.info('ZEROP ERR: %s', np.abs(zero_point - new_zero_point))
+
+        self.assertTrue(
+            np.allclose(new_scale, scale, atol=1e-2) and
+            np.allclose(new_zero_point, zero_point, atol=1e-2)
+        )
+
+    def test_scaled_quantization_and_dequantization(self):
+        logger.warning('test_scaled_quantization_and_dequantization')
+
+        r_tensor = torch.randn((4, 4))
+
+        quantized_tensor, new_scale, new_zero_point = linear_quantizer(r_tensor)
+        dequantized_tensor = linear_dequantizer(quantized_tensor, new_scale, new_zero_point)
+
+        plot_quantization_errors(r_tensor, quantized_tensor, dequantized_tensor)
+
+        error = (dequantized_tensor-r_tensor).square().mean()
+
+        logger.info('error: %s', error.numpy())
+
+        self.assertTrue(
+            np.allclose(error, 0.0, atol=1e-4)
+        )
 
 if __name__ == '__main__':
     unittest.main()
